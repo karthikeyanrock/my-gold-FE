4884ff04b5488043f3b21b68032424dd19bf31ae
karthikeyanrock <karthisaloki123@gmail.com>
2023-03-30 20:53:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Goldcalculator from "./components/Goldcalculator";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Goldcalculator />} />
+      
+        <Route path="/goldcalc" element={<Goldcalculator />} />
+       
+      </Routes>
+    </Router>
   );
 }
 
